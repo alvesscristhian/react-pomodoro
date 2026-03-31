32771cbca18b4c2646b9a212205e4276d5f59f67
Cristhian Alves <cristhian.ads@hotmail.com>
2026-03-31 03:03:05 -0300

    Aprendendo sobre componentes em React

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
+import { App } from "./App";
+
+createRoot(document.getElementById("root")!).render(
+  // Se o documento tiver esse elemento vai renderizar abaixo
+  // StrictMode - Roda o código 2x para garantir que o code vai funcionar, ao buildar não acontece mais
   <StrictMode>
     <App />
   </StrictMode>,
-)
+  // App - Utiliza o componente App ná página
+);
