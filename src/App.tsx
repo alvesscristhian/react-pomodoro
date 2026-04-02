e3f39a1daa7cf84940c1696d6a2f0bbbd1b3f972
Cristhian Alves <cristhian.ads@hotmail.com>
2026-04-02 00:47:34 -0300

    Tipagem do componente Heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Heading } from "./components/Heading";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import { TimerIcon } from "lucide-react";
 
 export function App() {
   console.log("test");
@@ -10,8 +11,11 @@ export function App() {
     // React Fragment: Vira elemento pai como se fosse uma <div>, possibilitando return de dois elementos juntos em um componente
     // Heading é o componente, os atributos entram como children também
     <>
-      <Heading attr={123} attr2="String">
-        Olá mundo
+      <Heading>
+        Olá mundo 1
+        <button>
+          <TimerIcon />
+        </button>
       </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis unde
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,8 +1,12 @@
 // Todo css que tiver .module.css são arquivos de escopo, não tendo acesso global
 import styles from "./Heading.module.css";
 
+// Seu tipo é um objeto
+type HeadingProps = {
+  children: React.ReactNode; // A chave vai aceitar tudo que o react como children
+};
+
 // Tudo que é colocado entre o componente no App vira o children (props)
-export function Heading(props) {
-  console.log(props);
-  return <h1 className={styles.heading}>{props.children}</h1>;
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
 }
