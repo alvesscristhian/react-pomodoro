49d023ad77b9dfaad06b7823cd00ced4eeebcf72
Cristhian Alves <cristhian.ads@hotmail.com>
2026-04-07 01:14:34 -0300

    Configurar classes css de container e content

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import { Heading } from "./components/Heading";
-
 import "./styles/theme.css";
 import "./styles/global.css";
-import { TimerIcon } from "lucide-react";
 
 export function App() {
-  console.log("test");
-
   return (
-    // React Fragment: Vira elemento pai como se fosse uma <div>, possibilitando return de dois elementos juntos em um componente
-    // Heading é o componente, os atributos entram como children também
     <>
-      <Heading>
-        Olá mundo 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis unde
-        voluptatum tempora enim vero saepe doloribus libero dicta perferendis.
-        Vitae officiis at optio nulla autem aperiam suscipit iure facere
-        corrupti.
-      </p>
+      <div className="container">
+        <div className="content">
+          <section>LOGO</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>MENU</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FORM</section>
+        </div>
+      </div>
+      <div className="container">
+        <div className="content">
+          <section>FOOTER</section>
+        </div>
+      </div>
     </>
   );
 }
