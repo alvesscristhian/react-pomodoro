8eae64d8c2af26d8e530ced97f2b7361f59804de
Cristhian Alves <cristhian.ads@hotmail.com>
2026-04-07 23:29:13 -0300

    Criar componente de logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,12 @@
+// Todo css que tiver .module.css são arquivos de escopo, não tendo acesso global
+import styles from "./styles.module.css";
+
+// Seu tipo é um objeto
+type LogoProps = {
+  children: React.ReactNode; // A chave vai aceitar tudo que o react como children
+};
+
+// Tudo que é colocado entre o componente no App vira o children (props)
+export function Logo({ children }: LogoProps) {
+  return <h1 className={styles.logo}>{children}</h1>;
+}
