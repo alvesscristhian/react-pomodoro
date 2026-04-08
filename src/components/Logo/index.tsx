b37ab248c589a79577f69bb2858c96c9af581b14
Seu Nome <seuemail@exemplo.com>
2026-04-08 00:18:44 -0300

    Adicionar svg e estilizar o componente de logo

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,12 +1,13 @@
 // Todo css que tiver .module.css são arquivos de escopo, não tendo acesso global
+import { TimerIcon } from "lucide-react";
 import styles from "./styles.module.css";
 
-// Seu tipo é um objeto
-type LogoProps = {
-  children: React.ReactNode; // A chave vai aceitar tudo que o react como children
-};
-
 // Tudo que é colocado entre o componente no App vira o children (props)
-export function Logo({ children }: LogoProps) {
-  return <h1 className={styles.logo}>{children}</h1>;
+export function Logo() {  
+  return <div className={styles.logo}>
+    <a className={styles.logoLink} href="#">
+      <TimerIcon />
+      <span>Chronos</span>
+    </a>
+  </div>;
 }
